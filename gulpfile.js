296704ae965a5dae68cb3aafbd6feb85e4ac59fe
eric chuderewicz <echuderewicz@gmail.com>
2018-04-04 20:06:18 -0400

    Lecture 16: completion. installed browser-sync and configured gulpfile.js to allow postcss to generate the file and then that is piped to browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
   console.log('Hooray');
@@ -22,12 +23,31 @@ gulp.task('styles', function(){
 });
 
 gulp.task('watch', function(){
+
+  browserSync.init({
+    //this removed the annoying/distracting black button
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  })
+
   watch('./app/index.html', function(){
-    gulp.start('html');
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('styles');
+    gulp.start('cssInject');
   })
 
 });
+
+//inject css into during
+//note the 2nd argument which indicates the dependencies
+//that need to run before cssInject
+
+gulp.task('cssInject', ['styles'], function(){
+  //the temp styles has be generated prior to piping into browser-sync
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
+})
