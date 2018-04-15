ca0b4524fbef4559583b0071b8232c4b41c7db67
eric chuderewicz <echuderewicz@gmail.com>
2018-04-15 15:24:50 -0400

    Lecture 35: completion

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
-rename = require('gulp-rename');
+rename = require('gulp-rename'),
+del = require('del');
 
 var config = {
 	mode: {
@@ -16,12 +17,18 @@ var config = {
 	}
 }
 
-gulp.task('createSprite', function(){
+gulp.task('createSprite', ['beginClean'], function(){
 	return gulp.src('./app/assets/images/icons/**/*.svg')
 	.pipe(svgSprite(config))
 	.pipe(gulp.dest('./app/temp/sprite/'));
 
 });
+
+gulp.task('beginClean', function(){
+
+	return del(['./app/temp/sprite', 'app/assets/images/sprites']);
+
+})
 
 gulp.task('copySpriteGraphic', ['createSprite'], function(){
 	return gulp.src('./app/temp/sprite/css/**/*.svg')
@@ -35,4 +42,8 @@ gulp.task('copySpriteCSS', ['createSprite'], function() {
 		.pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function(){
+	return del('./app/temp/sprite');
+})
+
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
