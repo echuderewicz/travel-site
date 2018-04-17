ea769d502178560d203088a63b15297f2f3cf13d
eric chuderewicz <echuderewicz@gmail.com>
2018-04-17 14:25:27 -0400

    Lecture 46: completion..got wapoints up and running

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 
 let mobileMenu = new MobileMenu();
+let revealOnScroll = new RevealOnScroll();
+
