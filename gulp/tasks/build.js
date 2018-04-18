3143208af803b17e119a8813dd23f4bd870cf6c0
eric chuderewicz <echuderewicz@gmail.com>
2018-04-18 17:42:19 -0400

    Lecture 60: completed the build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,10 +1,40 @@
 var gulp = require('gulp'),
 imagemin = require('gulp-imagemin'),
 del = require('del'),
-usemin = require('gulp-usemin');
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
 
-gulp.task("deleteDocsFolder", function(){
+gulp.task('previewDocs', function(){
+	browserSync.init({
+    //this removed the annoying/distracting black button
+    notify: false,
+    server: {
+      baseDir: "docs"
+    }
+  });
+})
+
+
+gulp.task("deleteDocsFolder", ['icons'], function(){
 	return del("./docs");
+})
+
+gulp.task('copyGeneralFiles', ['deleteDocsFolder'], function(){
+	var pathsToCopy = [
+		'./app/**/*',
+		'!./app/index.html',
+		'!./app/assets/images/**',
+		'!./app/assets/styles/**',
+		'!./app/assets/scripts/**',
+		'!./app/temp',
+		'!./app/temp/**'
+	];
+
+	return gulp.src(pathsToCopy)
+		.pipe(gulp.dest("./docs"));
 })
 
 gulp.task('optimizeImages', ['deleteDocsFolder'], function(){
@@ -17,11 +47,18 @@ gulp.task('optimizeImages', ['deleteDocsFolder'], function(){
 	.pipe(gulp.dest("./docs/assets/images"));
 })
 
-gulp.task('usemin', ['deleteDocsFolder'], function(){
+gulp.task('useminTrigger', ['deleteDocsFolder'], function(){
+	gulp.start("usemin");
+})
+
+gulp.task('usemin', ['styles', 'scripts'], function(){
 	return gulp.src("./app/index.html")
-	.pipe(usemin())
+	.pipe(usemin({
+		css: [function(){return rev()}, function(){return cssnano()}],
+		js: [function(){return rev()}, function(){return uglify()}]
+	}))
 	.pipe(gulp.dest("./docs"));
 })
 
 
-gulp.task('build', ['deleteDocsFolder', 'optimizeImages', 'usemin']);
+gulp.task('build', ['deleteDocsFolder', 'copyGeneralFiles', 'optimizeImages', 'useminTrigger']);
